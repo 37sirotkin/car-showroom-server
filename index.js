3ab56b64c960ffbfbec2c91496a50b57983a65de
asirotkin <salexvlad@gmail.com>
2021-03-25 21:15:32 +0300

    mark model

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 const pool = require("./db");
 
+const { Mark } = require('./src/models/mark');
+
 app.use(cors());
 app.use(express.json());
 
@@ -14,7 +16,7 @@ app.post("/post", async (req, res) => {
         const {mark, model, color, price, year_of_issue, id_supplier} = req.body;
         const newCar = await pool.query(
             "INSERT INTO car (mark, model, color, price, year_of_issue, id_supplier) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-            [mark, model, color, price, year_of_issue, id_supplier]);
+            [mark, model, color, price, year_of_issue, id_supplier]``);
         res.json(newCar.rows[0]);
     } catch (err) {
         console.log(err.message);
@@ -23,7 +25,9 @@ app.post("/post", async (req, res) => {
 
 app.get("/getCars", async (req, res) => {
     try {
-        const cars = await Car.findAll({});
+        const cars = await Car.findAll({
+            include: Mark
+        });
         res.json(cars.map(c => c.dataValues));
     } catch (err) {
         console.log(err.message);
--- src/models/car.js
+++ src/models/car.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const { sequelize } = require('./sequelize');
+const { Mark } = require('./mark');
 
 class Car extends Model {}
 
@@ -9,7 +10,6 @@ Car.init({
     autoIncrement: true,
     type:DataTypes.INTEGER
   },
-  mark: DataTypes.CHAR,
   model: DataTypes.CHAR,
   color: DataTypes.CHAR,
   price: DataTypes.CHAR,
@@ -21,6 +21,12 @@ Car.init({
   tableName: 'car', 
   createdAt: false, 
   updatedAt: false
+});
+
+Car.belongsTo(Mark, {
+  foreignKey: {
+    field: 'id_mark'
+  }
 });
 
 module.exports.Car = Car;
