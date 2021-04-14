b45283a82536c9c35db8aa5673f1ff73adc9f7b8
asirotkin <salexvlad@gmail.com>
2021-04-14 21:18:15 +0300

    swagger code generator

---
+++ src/api/routes/cars.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cars = require('../services/cars');
+
+const router = new express.Router();
+
+
+/**
+ * Retrieve cars
+ */
+router.get('/', async (req, res, next) => {
+  const options = {
+    markId: req.query['markId'],
+    color: req.query['color']
+  };
+
+  try {
+    const result = await cars.findCars(options);
+    res.status(result.status || 200).send(result.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
---
+++ src/api/services/cars.js
@@ -0,0 +1,30 @@
+const ServerError = require('../../lib/error');
+const {Car} = require('./../../models/car');
+
+/**
+ * @param {Object} options
+ * @param {Integer} options.markId Filters on specified mark id
+ * @param {String} options.color Filters on specified mark id
+ * @throws {Error}
+ * @return {Promise}
+ */
+module.exports.findCars = async (options) => {
+  const where = {};
+  if (!!options.markId) {
+    where.id_mark = options.markId;
+  }
+
+  if (!!options.color) {
+    where.color = options.color;
+  }
+
+  const cars = await Car.findAll({
+    where
+  });
+
+  return {
+    status: 200,
+    data: cars.map(c => c.dataValues)
+  };
+};
+
